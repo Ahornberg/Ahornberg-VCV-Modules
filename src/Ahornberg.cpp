50a5da7b5da8e529a84fcd3615d612d5b24b043d
Ahornberg <birgit.jauernig@yahoo.com>
2018-01-13 22:45:25 +0100

    initial commit

---
+++ src/Ahornberg.cpp
@@ -0,0 +1,23 @@
+#include "Ahornberg.hpp"
+
+
+// The plugin-wide instance of the Plugin class
+Plugin *plugin;
+
+void init(rack::Plugin *p) {
+	plugin = p;
+	// The "slug" is the unique identifier for your plugin and must never change after release, so choose wisely.
+	// It must only contain letters, numbers, and characters "-" and "_". No spaces.
+	// To guarantee uniqueness, it is a good idea to prefix the slug by your name, alias, or company name if available, e.g. "MyCompany-MyPlugin".
+	// The ZIP package must only contain one folder, with the name equal to the plugin's slug.
+	p->slug = "Ahornberg-Microtonal";
+	p->version = TOSTRING(VERSION);
+	p->website = "https://github.com/VCVRack/Ahornberg";
+	p->manual = "https://github.com/VCVRack/Ahornberg/blob/master/README.md";
+
+	// For each module, specify the ModuleWidget subclass, manufacturer slug (for saving in patches), manufacturer human-readable name, module slug, and module name
+	p->addModel(createModel<MicroTuner1Widget>("Ahornberg", "MicroTuner", "MicroTuner", TUNER_TAG));
+
+	// Any other plugin initialization may go here.
+	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
+}
