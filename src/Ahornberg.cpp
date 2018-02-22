d2c1b60582adc10360b0e13edb8f020578ad4c03
Phil Golden <12150421+Coirt@users.noreply.github.com>
2018-02-22 10:17:59 +0000

    v0.6.0dev

--- src/Ahornberg.cpp
+++ src/Ahornberg.cpp
@@ -1,7 +1,7 @@
 #include "Ahornberg.hpp"
 
 
-// The plugin-wide instance of the Plugin class
+/* The plugin-wide instance of the Plugin class
 Plugin *plugin;
 
 void init(rack::Plugin *p) {
@@ -12,12 +12,27 @@ void init(rack::Plugin *p) {
 	// The ZIP package must only contain one folder, with the name equal to the plugin's slug.
 	p->slug = "Ahornberg-Microtonal";
 	p->version = TOSTRING(VERSION);
+	
+	!---- NOT SURE WHERE THESE GO!
 	p->website = "https://github.com/Ahornberg/Ahornberg-Microtonal";
 	p->manual = "https://github.com/Ahornberg/Ahornberg-Microtonal";
-
+	!---- NOT SURE WHERE THESE GO!
+	
 	// For each module, specify the ModuleWidget subclass, manufacturer slug (for saving in patches), manufacturer human-readable name, module slug, and module name
 	p->addModel(createModel<EqualDivisionWidget>("Ahornberg", "EqualDivision", "EqualDivision", TUNER_TAG));
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
+}
+*/
+
+Plugin *pluginAhornberg;
+
+void init(rack::Plugin *p) {
+	pluginAhornberg = p;
+	p->slug = TOSTRING(SLUG);
+	p->version = TOSTRING(VERSION);
+
+	//p->addModule(model NAME_OF Module);
+	p->addModel(modelEqualDivisionModule);
 }
