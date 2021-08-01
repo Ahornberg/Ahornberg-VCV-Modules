3cf14240195527443bb24f1213a311dcfd5073de
Ahornberg <birgit.jauernig@yahoo.com>
2021-08-01 16:36:33 +0200

    idea for tracker module

--- src/modules/Tracker/TrackerWidget.cpp
+++ src/modules/Tracker/TrackerWidget.cpp
@@ -7,8 +7,15 @@ TrackerScreen::TrackerScreen(Rect box, Tracker* module) : SizedTransparentWidget
 
 void TrackerScreen::draw(const DrawArgs& disp) {
 	nvgBeginPath(disp.vg);
+	nvgRect(disp.vg, 0, 0, box.size.x, box.size.y);
+	nvgFillColor(disp.vg, COLOR_BLACK);
+	nvgFill(disp.vg);
+	nvgBeginPath(disp.vg);
+	nvgRect(disp.vg, 0, 0, box.size.x, 11.875);
+	nvgFillColor(disp.vg, COLOR_BROWN);
+	nvgFill(disp.vg);
 	nvgFillColor(disp.vg, COLOR_GREEN_DARK);
-	nvgFontSize(disp.vg, 14);
+	nvgFontSize(disp.vg, 12);
 	nvgFontFaceId(disp.vg, font->handle);
 	//nvgTextLetterSpacing(disp.vg, 1);
 	for (auto i = 10; i <= 380; i += 10) {
@@ -17,16 +24,15 @@ void TrackerScreen::draw(const DrawArgs& disp) {
 		} else {
 			nvgFillColor(disp.vg, COLOR_YELLOW);
 		}
-		// nvgText(disp.vg, 10, i * 1.054 - 1, (std::to_string(i / 10) + " OgOgOgOg").c_str(), NULL);
-		// 36 lines
-		nvgText(disp.vg, 10, i * 1.1875 - 2, (std::to_string(i / 10) + "---OgOgOgOg").c_str(), NULL);
-		// nvgText(disp.vg, 10, i * 1.583 - 2, (std::to_string(i / 10) + " OgOgOgOg").c_str(), NULL);
+		// nvgText(disp.vg, 10, i * 1.1875 - 2.25, (std::to_string(i / 10) + "---\u02DCOgOgOgOg").c_str(), NULL);
+		nvgText(disp.vg, 10, i * 1.1875 - 2.25, (std::to_string(i / 10) + "---\u02DC\u02C7~\u02DCOgOgOgOg").c_str(), NULL);
+		nvgText(disp.vg, 10, i * 1.1875 - 2.25, (std::to_string(i / 10) + "---vpfrOgOgOgOg").c_str(), NULL);
 	}
 }
 
 TrackerWidget::TrackerWidget(Tracker* module) {
 	setModule(module);
-	setPanel("res/Tracker.svg");
+	// setPanel("res/Tracker.svg");
 	setWidthInHP(12);
 
 	addChild(new TrackerScreen(Rect(0, 0, 180, 380), module));
